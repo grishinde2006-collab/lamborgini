cdebc7ca89f5d5d507a6e71bb00b50ab9048338e
grishinde2006-collab <grishinde2006@gmail.com>
2026-05-18 23:36:02 +0300

    Update settings.py

--- myproject/settings.py
+++ myproject/settings.py
@@ -80,10 +80,11 @@ TEMPLATES = [
 WSGI_APPLICATION = 'myproject.wsgi.application'
 
 DATABASES = {
-    'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
-    }
+    'default': dj_database_url.config(
+        default=os.environ.get('DATABASE_URL'),
+        conn_max_age=600,
+        conn_health_checks=True,
+    )
 }
 
 AUTH_PASSWORD_VALIDATORS = [
