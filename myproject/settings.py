e6f9ed103946ce916c163f320a977e3742a38777
grishinde2006-collab <grishinde2006@gmail.com>
2026-05-19 15:23:16 +0300

    Update settings.py

--- myproject/settings.py
+++ myproject/settings.py
@@ -1,5 +1,13 @@
 import os
 from pathlib import Path
+import dj_database_url
+
+DATABASES = {
+    'default': dj_database_url.config(
+        default=os.environ.get('DATABASE_URL'),
+        conn_max_age=600
+    )
+}
 
 # ========== НАСТРОЙКА МИКРОСЕРВИСОВ ==========
 SERVICE_ROLE = os.environ.get('SERVICE_ROLE', 'full')
