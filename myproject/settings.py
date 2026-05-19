fcf173b91580f220534d2a7e092db6963a8ca0b3
grishinde2006-collab <grishinde2006@gmail.com>
2026-05-19 15:30:53 +0300

    Update settings.py

--- myproject/settings.py
+++ myproject/settings.py
@@ -1,13 +1,10 @@
 import os
 from pathlib import Path
+import dj_database_url
 
 # ========== НАСТРОЙКА МИКРОСЕРВИСОВ ==========
 SERVICE_ROLE = os.environ.get('SERVICE_ROLE', 'full')
 # =============================================
-
-"""
-Django settings for myproject project.
-"""
 
 BASE_DIR = Path(__file__).resolve().parent.parent
 
@@ -74,28 +71,20 @@ TEMPLATES = [
 
 WSGI_APPLICATION = 'myproject.wsgi.application'
 
-# ========== БАЗА ДАННЫХ ==========
+# ========== БАЗА ДАННЫХ POSTGRESQL (ОБЩАЯ) ==========
 DATABASES = {
-    'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
-    }
+    'default': dj_database_url.config(
+        default=os.environ.get('DATABASE_URL'),
+        conn_max_age=600
+    )
 }
-# =================================
+# ====================================================
 
 AUTH_PASSWORD_VALIDATORS = [
-    {
-        'NAME': 'django.contrib.auth.password_validation.UserAttributeSimilarityValidator',
-    },
-    {
-        'NAME': 'django.contrib.auth.password_validation.MinimumLengthValidator',
-    },
-    {
-        'NAME': 'django.contrib.auth.password_validation.CommonPasswordValidator',
-    },
-    {
-        'NAME': 'django.contrib.auth.password_validation.NumericPasswordValidator',
-    },
+    {'NAME': 'django.contrib.auth.password_validation.UserAttributeSimilarityValidator'},
+    {'NAME': 'django.contrib.auth.password_validation.MinimumLengthValidator'},
+    {'NAME': 'django.contrib.auth.password_validation.CommonPasswordValidator'},
+    {'NAME': 'django.contrib.auth.password_validation.NumericPasswordValidator'},
 ]
 
 LANGUAGE_CODE = 'en-us'
